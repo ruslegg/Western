03bf45007c8ac43cdbb038db37bf0b88f9d6992e
ruslegg <negrei.ruslan@gmail.com>
2017-10-19 15:36:58 +0200

    Connected FXML Connected CSS Basic UI Basic Design Change scenes functionality

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,23 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1000, 1000));
-        primaryStage.setResizable(false);
-        primaryStage.show();
-        System.out.println("Test2");
+        try {
+            GridPane root = FXMLLoader.load(getClass().getResource("FXML/main.fxml"));
+            Scene scene = new Scene(root,626, 590);
+            scene.getStylesheets().add("/css/main.css");
+            root.getStyleClass().add("mainbackground");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 
