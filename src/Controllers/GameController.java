62858d5dd1fa8d62ff925f94e3fbb285817f47e3
ruslegg <negrei.ruslan@gmail.com>
2017-10-23 12:37:10 +0200

    MainGame Scene Controller+CSS+FXML

---
+++ src/Controllers/GameController.java
@@ -0,0 +1,22 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+
+public class GameController {
+    @FXML
+    Button answerButton1,answerButton2,answerButton3,answerButton4,doorButton,exitButton;
+    @FXML
+    Label answerLabel1,answerLabel2,answerLabel3,answerLabel4,questionNumberLabel,clockLabel,questionLabel;
+    @FXML
+    ImageView questionPersonImage;
+
+    public void newQuestion(){
+
+    }
+
+}
+
