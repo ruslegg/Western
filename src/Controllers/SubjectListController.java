b6fa9c0409e8c61632d4f408569f09eb16e4c15b
ruslegg <negrei.ruslan@gmail.com>
2017-10-23 14:27:13 +0200

    added SubjectListController + FXML + CSS

---
+++ src/Controllers/SubjectListController.java
@@ -0,0 +1,13 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class SubjectListController {
+    @FXML
+    TableView tableView;
+    @FXML
+    Button backButton,playButton;
+
+}
