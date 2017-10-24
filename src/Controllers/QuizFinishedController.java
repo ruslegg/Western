f37ea83c5b706e37f0203af8a59c29ba90fc2b98
Andrei <yungserho@gmail.com>
2017-10-24 13:11:21 +0200

    connect frames, functionality to buttons most of them, fix errors, fix bugs, mysql working

---
+++ src/Controllers/QuizFinishedController.java
@@ -0,0 +1,57 @@
+package Controllers;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import javax.swing.text.html.ImageView;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class QuizFinishedController implements Initializable {
+    private Stage stage;
+    @FXML
+    public Label congratulationLabel;
+    @FXML
+    public Label scoreLabel;
+    @FXML
+    public Label answerRateLabel;
+    @FXML
+    public Button toLeaderboardsButton;
+    @FXML
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    public void Congratulation() throws IOException {
+    }
+
+    public void Score() throws IOException {
+
+    }
+
+    public void answerRate() throws IOException {
+
+    }
+
+    public void ToLeaderboards(MouseEvent event) throws IOException{
+        stage = (Stage) toLeaderboardsButton.getScene().getWindow();
+        Pane root;
+        root = FXMLLoader.load(getClass().getResource("/FXML/leaderboards.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/css/leaderboards.css");
+        stage.setScene(scene);
+        System.out.println("ToLeaderboards");
+    }
+}
