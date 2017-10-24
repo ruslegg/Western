6800668f21b139135f048ca6309e1101e10df6cf
Andrei <yungserho@gmail.com>
2017-10-24 13:43:16 +0200

    added Briefing Controller and fxml.

---
+++ src/Controllers/BriefingController.java
@@ -0,0 +1,46 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import javax.swing.text.html.ImageView;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+// need timer
+public class BriefingController implements Initializable {
+    private Stage stage;
+    @FXML
+    public Label cityLabel;
+    @FXML
+    public ImageView briefingNPC;
+    @FXML
+    public Label textBriefing;
+
+
+    public void changeScene() throws IOException {
+            stage = (Stage) cityLabel.getScene().getWindow();
+            Pane root;
+            root = FXMLLoader.load(getClass().getResource("/FXML/game.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add("/css/game.css");
+            stage.setScene(scene);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            changeScene();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
