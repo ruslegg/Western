f54ee6864fdede3448b48110fbd47fd3e0c33a4c
Andrei <yungserho@gmail.com>
2017-10-26 13:46:11 +0200

    Added music in LoginController.java

--- src/Controllers/LoginController.java
+++ src/Controllers/LoginController.java
@@ -47,7 +47,7 @@ public class LoginController {
     private Stage stage;
 
     public void music() throws IOException, SQLException, UnsupportedAudioFileException, LineUnavailableException {
-        AudioInputStream as1 = AudioSystem.getAudioInputStream(new java.io.FileInputStream("cumbusters.wav"));
+        AudioInputStream as1 = AudioSystem.getAudioInputStream(new java.io.FileInputStream("Cumbusters.wav"));
         AudioFormat af = as1.getFormat();
         Clip clip1 = AudioSystem.getClip();
         DataLine.Info info = new DataLine.Info(Clip.class, af);
