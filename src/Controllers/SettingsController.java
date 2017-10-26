135b60e159744f25d5312d228b1a3171ce8d8729
Andrei <yungserho@gmail.com>
2017-10-26 17:59:17 +0200

    added MusicController, music works very cool but doesn't stop unf. Main java start music loop

--- src/Controllers/SettingsController.java
+++ src/Controllers/SettingsController.java
@@ -9,12 +9,19 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import sun.audio.AudioPlayer;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 
 public class SettingsController implements Initializable {
+    private boolean prev = true;
+
     private Stage stage;
     @FXML
     public CheckBox soundButton;
@@ -31,9 +38,13 @@ public class SettingsController implements Initializable {
     public void soundTrigger(MouseEvent event) throws IOException {
 
     }
-    public void musicTrigger(MouseEvent event) throws  IOException{
 
+    public void musicTrigger(MouseEvent event) throws IOException, UnsupportedAudioFileException, SQLException, LineUnavailableException {
+        MusicController music = new MusicController();
+        prev = !prev;
+        music.playMusic(prev);
     }
+
     public void toMainMenu(MouseEvent event) throws IOException{
         stage = (Stage) backButton.getScene().getWindow();
         Pane root;
