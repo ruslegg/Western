01709bf3d63add809e410f5d36d545dd1ede27d7
renegage236 <alexgbalan5@gmail.com>
2017-11-13 10:50:11 +0100

    add user.txt

--- src/Model/Teacher.java
+++ src/Model/Teacher.java
@@ -1,12 +1,38 @@
 package Model;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 public class Teacher extends User {
     boolean isTeacher;
     Teacher(){
 
     }
-    Teacher(String name,boolean isTeacher){
+    public Teacher(String name,boolean isTeacher){
         this.name=name;
         this.isTeacher=isTeacher;
     }
+
+    @Override
+    public void serialize() throws IOException {
+        Writer wr = new FileWriter("src/data/users/user.txt",true);
+        BufferedWriter bw = new BufferedWriter(wr);
+        bw.write(name);
+        bw.write(" ");
+        bw.write(String.valueOf(isTeacher));
+        bw.newLine();
+        bw.close();
+    }
+
+    public boolean isTeacher() {
+        return isTeacher;
+    }
+
+    public void setTeacher(boolean teacher) {
+        isTeacher = teacher;
+    }
+
+
 }
